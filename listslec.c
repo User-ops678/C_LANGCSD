1b40d853bd2adcf776037ab9416b214d2ff8ae88
Nikos <nikolonikakos03@gmail.com>
2024-12-10 12:03:34 +0200

    LECTURE #21 LISTS EXAMPLES (SEG FAULT (THIS IS TO SEE WHAT COMMANDS TO USE)!

--- listslec.c
+++ listslec.c
@@ -1,5 +1,8 @@
 #include<stdio.h>
+#include<stdlib.h>
 #include<assert.h>
+#include<string.h>
+
 
 typedef struct item {
 	int data;
@@ -12,7 +15,7 @@ list_t* empty_list() {
 list_t* addFront(list_t* head, int data) {
 	list_t* new = malloc(sizeof(list_t)); //struct item* new = malloc(sizeof(struct item));
 	if (new == NULL) {
-		fpritnf(stderr, "Memory allocation failed!\n");
+		fprintf(stderr, "Memory allocation failed!\n");
 		exit(EXIT_FAILURE);
 	}
 	new->data = data;
@@ -44,15 +47,102 @@ list_t* addLast(list_t* head, int data) {
 	head->next = addLast(head->next,data);
 	return head;
 }
-int main() {
-	list_t* list = empty_list();
-	list = add_front(list, 42);
-	list = add_front(list, 4);
-	list = add_front(list, 2);
-	list = add_front(list, 5);
-	list = add_front(list, 100);
+
+
+list_t* find(list_t* head, int data) {
+	for(list_t* l = head; l != NULL && l->data != data; l = l->next) {
+	//return l;
+		if(l->data == data) return l;
+	}
+	//return NULL;
+}
+
+list_t *removeFront(list_t* head) {
+	list_t* tail = head->next;
+	free(head);
+	return tail;
+}
+//Cannot be called on an empty list
+void removeAfter(list_t* head) {
+	head->next = removeFront(head->next);
+}
+
+list_t* removeElt(list_t* head, list_t *elt) {
+	if (head == elt) {
+		return removeFront(head);
+	}
+	if (head == NULL) {
+		return NULL;
+	}
+	//Tropos me epanalhpsi:
+	//list_t* l = head;
+	//for(l = head; l && l->next == elt; l = l->next);
+	//list_t* d = l->next
+	//l->next = l->next->next;
+	//free(d);
+	//return head;
+	//Anadromikos tropos:
+	head->next = removeElt(head->next, elt);
+	return head;
+}
+
+void printList(list_t* head) {
+	list_t* l;
+	for(l = head; l; l = l->next) {
+		printf("%d -> ", l->data);
+	}
+	printf("NULL\n");
+}
+
+void list_to_file(list_t* head, FILE *f) {
+	rewind(f);
+	list_t* l;
+	for(l = head; l; l = l->next) {
+		fwrite(&l->data, sizeof(int), 1, f);
+	}
+}
+
+list_t* list_from_file(FILE *f) {
+	list_t* head = empty_list();
+	int data;
+	for(;;) {
+		fread(&data, sizeof(int), 1, f);
+		if (!feof(f)) break;
+		head = addFront(head, data);
+	}
+	return head;
+}
+
+int main(int argc, char *argv[]) {
+	list_t* ptr;
+	FILE *f;
+	f = fopen("numbers.dat", "rb+");
+	if (f == NULL) {
+		return -2;
+	}
+	list_t* list;
+	//list_t* list = empty_list();
+	//list = addFront(list, 42);
+	//list = addFront(list, 4);
+	//list = addFront(list, 2);
+	//list = addFront(list, 5);
+	//list = addFront(list, 100);
 	//list = add_front(list, 42);
 	//list = add_front(list, 42);
 	//list = add_front(list, 42);
 	//list = add_front(list, 42);
+	//ptr = find(list, 42);
+	//printList(list);
+
+	if(argc >= 2) {
+		if(strcmp(argv[1], "--insert") == 0) {
+			list = addLast(list, 22);
+		}
+		if(strcmp(argv[1], "--delete") == 0) {
+			removeAfter(list);
+		}
+		list_to_file(list, f);
+		printList(list);
+		list = list_from_file(f);
+	}
 }
